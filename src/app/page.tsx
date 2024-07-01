06d0539d58db3868fa226d58a0722afae087bc0a
= <=>
2024-07-01 13:33:06 -0700

    Add two images, add images to root page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
 import React from "react";
-import { prefix } from './prefix';
 import ArduinoIDEScreenshot from '../../public/softwarePhotos/Arduino IDE Screenshot.png'
+import PCBScreenshot from '../../public/hardwarePhotos/PCB Screenshot Example.png'
+import ThreeDModelingScreenshot from '../../public/CADPhotos/3d Modeling Screenshot.png'
+import Mk3SupraEngineBay from '../../public/1991 Supra Engine Bay.jpg'
 
 export default async function Home() {
     console.log(process.env.NEXT_PUBLIC_ENV);
@@ -26,7 +28,7 @@ export default async function Home() {
       <Link href="/hardware" className="m-10">
           <div className="card w-96 bg-neutral shadow-xl z-0">
               <figure className="h-1/2 z-0">
-                  <Image src={`${prefix}/hardwarePhotos/PCB Screenshot Example.png`} alt="Screenshot of PCB 3D Model" width={500} height={500}/>
+                  <Image src={PCBScreenshot} alt="Screenshot of PCB 3D Model" width={500} height={500}/>
               </figure>
               <div className="h-1/2 card-body z-0">
                   <h2 className="card-title z-0">Hardware Projects</h2>
@@ -37,7 +39,7 @@ export default async function Home() {
       <Link href="/3dModeling" className="m-10">
           <div className="card w-96 bg-neutral shadow-xl z-0">
               <figure className="h-1/2 z-0">
-                  <Image src={`${prefix}/CADPhotos/3d Modeling Screenshot.png`} alt="Screenshot of 3D CAD Model" width={500} height={500}/>
+                  <Image src={ThreeDModelingScreenshot} alt="Screenshot of 3D CAD Model" width={500} height={500}/>
               </figure>
               <div className="h-1/2 card-body z-0">
                   <h2 className="card-title z-0">CAD Projects</h2>
@@ -48,7 +50,7 @@ export default async function Home() {
       <Link href="" className="m-10">
           <div className="card w-96 bg-neutral shadow-xl z-0">
               <figure className="h-1/2 z-0">
-                  <Image src={``} alt="1991 Toyota Supra" width={500} height={500}/>
+                  <Image src={Mk3SupraEngineBay} alt="1991 Toyota Supra" width={500} height={500}/>
               </figure>
               <div className="h-1/2 card-body z-0">
                   <h2 className="card-title z-0">Other Projects</h2>
--- src/app/prefix.ts
+++
@@ -1,3 +0,0 @@
-const prefix = process.env.NEXT_PUBLIC_ENV === 'production' ? '/KamiHobbies' : '';
-
-export { prefix };
